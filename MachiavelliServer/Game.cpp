bad2217adef4ef19bbc53eec9ba9b92a9ed4fffd
luukspierings <lcpspier@avans.nl>
2017-12-29 00:23:06 +0100

    Added message when other players quit

--- MachiavelliServer/Game.cpp
+++ MachiavelliServer/Game.cpp
@@ -3,11 +3,16 @@
 
 void Game::addClient(shared_ptr<ClientInfo> client)
 {
-	for (auto& otherClient : clients) {
-		otherClient->get_player().notify("Player '" + client->get_player().get_name() + "' joined the game.");
-	}
+	notifyAllPlayers("Player '" + client->get_player().get_name() + "' has joined the game.");
 
 	clients.push_back(client);
+}
+
+void Game::notifyAllPlayers(string message)
+{
+	for (auto& otherClient : clients) {
+		otherClient->get_player().notify(message);
+	}
 }
 
 void Game::removeClient(ClientInfo & client)
@@ -16,6 +21,7 @@ void Game::removeClient(ClientInfo & client)
 	{
 		if ((*it)->get_socket().get_socket() == client.get_socket().get_socket()) {
 			it = clients.erase(it);
+			notifyAllPlayers("Player '" + client.get_player().get_name() + "' has left the game.");
 		}
 		else {
 			++it;
